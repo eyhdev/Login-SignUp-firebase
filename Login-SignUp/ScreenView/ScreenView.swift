dd8eb29acf0495eabee9fc16631d397469417aff
eyhdev <alaqsa335@gmail.com>
2024-01-21 17:04:56 +0300

    Login-SignUp

---
+++ Login-SignUp/ScreenView/ScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenView.swift
+//  Login-SignUp
+//
+//  Created by eyh.mac on 21.01.2024.
+//
+
+import SwiftUI
+
+struct ScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenView()
+}
