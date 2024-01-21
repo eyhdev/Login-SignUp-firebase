9ebfcd22a292f8e9a983915282017a127886bc62
eyhdev <alaqsa335@gmail.com>
2024-01-21 10:51:56 +0300

    Initial Commit

---
+++ Login-SignUp/Login_SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_SignUpApp.swift
+//  Login-SignUp
+//
+//  Created by eyh.mac on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Login_SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
